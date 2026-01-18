6c5f012c6e34b479d7423e797ffffa40fc365fbf
1F616EMO <root@1f616emo.xyz>
2026-01-18 22:24:32 +0800

    Add stereo sound to Leviathan album

--- src/001-leviathan.lua
+++ src/001-leviathan.lua
@@ -24,74 +24,33 @@ local album = phonograph.register_album("phonograph_album_john_oestmann:001-levi
     license = phonograph.licenses.CC0,
 })
 
-album:register_song("0x2A721", {
-    title = S("Lake Aria"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A721"),
-    spec = {},
-})
+local songs = {
+    {"2A721", "Lake Aria"},
+    {"2A722", "Rachel's Weave"},
+    {"2A723", "Sun Cave Village"},
+    {"2A724", "Skypole Gorge"},
+    {"2A725", "Moon Cave"},
+    {"2A726", "Orion's Geology Workshop"},
+    {"2A727", "Forgotten Shrine in the Forest"},
+    {"2A728", "City of the Leviathan"},
+}
 
-album:register_song("0x2A722", {
-    title = S("Rachel’s Weave"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A722"),
-    spec = {},
-})
-
-album:register_song("0x2A723", {
-    title = S("Sun Cave Village"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A723"),
-    spec = {},
-})
-
-album:register_song("0x2A724", {
-    title = S("Skypole Gorge"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A724"),
-    spec = {},
-})
-
-album:register_song("0x2A725", {
-    title = S("Moon Cave"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A725"),
-    spec = {},
-})
-
-album:register_song("0x2A726", {
-    title = S("Orion’s Geology Workshop"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A726"),
-    spec = {},
-})
-
-album:register_song("0x2A727", {
-    title = S("Forgotten Shrine in the Forest"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A727"),
-    spec = {},
-})
-
-album:register_song("0x2A728", {
-    title = S("City of the Leviathan"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A728"),
-    spec = {},
-})
+for _, song in ipairs(songs) do
+    album:register_song("0x" .. song[1], {
+        title = S(song[2]),
+        short_description = nil,
+        long_description = nil,
+        artist = nil,
+        spec = {
+            filepath = songpath(song[1]),
+        },
+        multichannel_specs = {
+            {
+                filepath = songpath(song[1] .. "_ch0"),
+            },
+            {
+                filepath = songpath(song[1] .. "_ch1"),
+            },
+        },
+    })
+end
