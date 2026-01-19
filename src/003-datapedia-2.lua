d1688e86a9a0a124f8bf65050537cbeb6dec889d
1F616EMO <root@1f616emo.xyz>
2026-01-19 17:27:04 +0800

    Add stereo soundtracks for Datapedia 2

--- src/003-datapedia-2.lua
+++ src/003-datapedia-2.lua
@@ -24,247 +24,51 @@ local album = phonograph.register_album("phonograph_album_john_oestmann:003-data
     license = phonograph.licenses.CC0,
 })
 
-album:register_song("0x2A73B", {
-    title = S("Gestalt Canals"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A73B"),
-    spec = {},
-})
-
-album:register_song("0x2A73C", {
-    title = S("Double Prism Ark"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A73C"),
-    spec = {},
-})
-
-album:register_song("0x2A73D", {
-    short_title = S("@1 [fragment]", "0x2A73D"),
-    title = S("@1 [discovery_fragment]", "0x2A73D"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A73D"),
-    spec = {},
-})
-
-album:register_song("0x2A72B-ii", {
-    short_title = S("Salanth Town Gardens 2"),
-    title = S("Salanth Town Gardens (Page 2)"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A72B-ii"),
-    spec = {},
-})
-
-album:register_song("0x2A73E", {
-    title = S("Myra Megafortress Ruins"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A73E"),
-    spec = {},
-})
-
-album:register_song("2A73F", {
-    title = S("-PRL Lobby (0A)"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A73F"),
-    spec = {},
-})
-
-album:register_song("0x2A740", {
-    title = S("The Falling Palace"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A740"),
-    spec = {},
-})
-
-album:register_song("0x2A741", {
-    title = S("Shrine of Zadra"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A741"),
-    spec = {},
-})
-
-album:register_song("0x2A741-ii", {
-    short_title = S("Shrine of Zadra 2"),
-    title = S("Shrine of Zadra (Page 2)"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A741-ii"),
-    spec = {},
-})
-
-album:register_song("0x2A742", {
-    title = S("Magma Refinery"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A742"),
-    spec = {},
-})
-
-album:register_song("0x2A742-ii", {
-    short_title = S("Magma Refinery 2"),
-    title = S("Magma Refinery (Page 2)"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A742-ii"),
-    spec = {},
-})
-
-album:register_song("0x2A743", {
-    short_title = S("@1 [fragment]", "0x2A743"),
-    title = S("@1 [discovery_fragment]", "0x2A743"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A743"),
-    spec = {},
-})
-
-album:register_song("0x2A72B-iii", {
-    short_title = S("Salanth Town Gardens 2"),
-    title = S("Salanth Town Gardens (Page 2)"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A72B-iii"),
-    spec = {},
-})
-
-album:register_song("0x2A744", {
-    title = S("Tomb of Zilst"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A744"),
-    spec = {},
-})
-
-album:register_song("0x2A745", {
-    title = S("Firebloom Academy"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A745"),
-    spec = {},
-})
-
-album:register_song("0x2A738-ii", {
-    short_title = S("Threadwalkers Tour Club 2"),
-    title = S("Threadwalkers Tour Club (Page 2)"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A738-ii"),
-    spec = {},
-})
-
-album:register_song("0x2A746", {
-    short_title = S("@1 [intercept]", "0x2A746"),
-    title = S("@1 [radio_intercept]", "0x2A746"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A746"),
-    spec = {},
-})
-
-album:register_song("0x2A747", {
-    short_title = S("Hyperway Stn. Construction Zone"),
-    title = S("Hyperway Station Construction Zone"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A747"),
-    spec = {},
-})
-
-album:register_song("0x2A747-ii", {
-    short_title = S("Hyperway Stn. Construction Zone 2"),
-    title = S("Hyperway Station Construction Zone (Page 2)"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A747-ii"),
-    spec = {},
-})
-
-album:register_song("0x2A748", {
-    title = S("Forbidden Swamps of Sanctuary"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A748"),
-    spec = {},
-})
-
-album:register_song("0x2A749", {
-    short_title = S("@1 [fragment]", "0x2A749"),
-    title = S("@1 [discovery_fragment]", "0x2A749"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A749"),
-    spec = {},
-})
-
-album:register_song("0x2A74A", {
-    title = S("Radio Fusayne"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A74A"),
-    spec = {},
-})
-
-album:register_song("0x2A74B", {
-    title = S("Windfields of Leviathan"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A74B"),
-    spec = {},
-})
-
-album:register_song("0x2A74C", {
-    title = S("Repeater Amon"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A74C"),
-    spec = {},
-})
-
-album:register_song("0x2A74D", {
-    title = S("Azurus Biochain"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A74D"),
-    spec = {},
-})
-
-album:register_song("0x2A74E", {
-    title = S("Repeater Babo"),
-    short_description = nil,
-    long_description = nil,
-    artist = nil,
-    filepath = songpath("2A74E"),
-    spec = {},
-})
+for _, data in ipairs({
+    -- ID, long_description (title), short_description (may be nil or string)
+    { "2A73B",     S("Gestalt Canals"),                              nil },
+    { "2A73C",     S("Double Prism Ark"),                            nil },
+    { "2A73D",     S("@1 [fragment]", "0x2A73D"),                    S("@1 [discovery_fragment]", "0x2A73D") },
+    { "2A72B-ii",  S("Salanth Town Gardens 2"),                      S("Salanth Town Gardens (Page 2)") },
+    { "2A73E",     S("Myra Megafortress Ruins"),                     nil },
+    { "2A73F",     S("-PRL Lobby (0A)"),                             nil },
+    { "2A740",     S("The Falling Palace"),                          nil },
+    { "2A741",     S("Shrine of Zadra"),                             nil },
+    { "2A741-ii",  S("Shrine of Zadra (Page 2)"),                    S("Shrine of Zadra 2") },
+    { "2A742",     S("Magma Refinery"),                              nil },
+    { "2A742-ii",  S("Magma Refinery (Page 2)"),                     S("Magma Refinery 2") },
+    { "2A743",     S("@1 [discovery_fragment]", "0x2A743"),          S("@1 [fragment]", "0x2A743") },
+    { "2A72B-iii", S("Salanth Town Gardens (Page 2)"),               S("Salanth Town Gardens 2") },
+    { "2A744",     S("Tomb of Zilst"),                               nil },
+    { "2A745",     S("Firebloom Academy"),                           nil },
+    { "2A738-ii",  S("Threadwalkers Tour Club (Page 2)"),            S("Threadwalkers Tour Club 2") },
+    { "2A746",     S("@1 [radio_intercept]", "0x2A746"),             S("@1 [intercept]", "0x2A746") },
+    { "2A747",     S("Hyperway Station Construction Zone"),          S("Hyperway Stn. Construction Zone") },
+    { "2A747-ii",  S("Hyperway Station Construction Zone (Page 2)"), S("Hyperway Stn. Construction Zone 2") },
+    { "2A748",     S("Forbidden Swamps of Sanctuary"),               nil },
+    { "2A749",     S("@1 [discovery_fragment]", "0x2A749"),          S("@1 [fragment]", "0x2A749") },
+    { "2A74A",     S("Radio Fusayne"),                               nil },
+    { "2A74B",     S("Windfields of Leviathan"),                     nil },
+    { "2A74C",     S("Repeater Amon"),                               nil },
+    { "2A74D",     S("Azurus Biochain"),                             nil },
+    { "2A74E",     S("Repeater Babo"),                               nil },
+}) do
+    local id, title, short_description = unpack(data)
+    album:register_song("0x" .. id, {
+        title = title,
+        short_description = short_description,
+        long_description = nil,
+        artist = nil,
+        spec = {
+            filepath = songpath(id),
+        },
+        multichannel_specs = {
+            {
+                filepath = songpath(id .. "_ch0"),
+            },
+            {
+                filepath = songpath(id .. "_ch1"),
+            },
+        },
+    })
+end
